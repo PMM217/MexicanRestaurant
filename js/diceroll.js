502bf2ddeb81427b34840a990e96dd3d2b99ce5a
s-dunne <165317980+s-dunne@users.noreply.github.com>
2024-04-26 18:06:07 +0100

    Update to drink and about page

--- js/diceroll.js
+++ js/diceroll.js
@@ -1,8 +1,18 @@
 //The user clicks on the button and gets a random number generated between 0 and 9. If the number is 5, they win and are given a randomized set of digits as a voucher.
 //If the number is not 5, they get an alert that says "Sorry, you have not won a free drink. You are welcome to try again. Good luck!"
 
+
+var round = 0; //variable to keep track of how many rounds the user has played, in order to limit it to 3
+
+
 //This function is used when the user clicks on the button.
 function playGame() {
+	
+	if (round > 2) {
+		alert("You have played your 3 rounds for today. Come back tomorrow.")
+	}
+	
+	else{
 	
 	const rando = Math.floor(Math.random() * 9);
 	
@@ -13,6 +23,10 @@ function playGame() {
     } else {
         alert("Sorry, you have not won a free drink. You are welcome to try again. Good luck!");
     }
+	
+	round++;
+	
+	}
 }
 //This function is used when the user wins the game by getting a 5. The voucher is randomly generated and displayed on an alert.
 function generateVoucher(length) {
